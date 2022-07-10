eea9b7b1b83f69138c4f34a8503a655b781340a3
yueb2 <yueb2@naver.com>
2022-07-10 16:27:25 +0900

    2022-07-10_hello_world

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello World! by YEB</p>
+        {/*
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+  */}
       </header>
     </div>
   );
